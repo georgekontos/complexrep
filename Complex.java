81085222c155a41a9092dade32392aac3f248bb1
George Kontos <george.kontos@sgdigital.com>
2018-12-03 10:54:05 +0000

    first commit

---
+++ Complex.java
@@ -0,0 +1,22 @@
+/*
+ * Represents a complex number
+ */
+class Complex {
+	private double j;
+	private double i;
+
+	//constructor
+	Complex(double j, double i) {
+		this.j = j;
+		this.i = i;
+	}
+	
+	//printme
+	public String toString() {
+		return j + " " + i + "i";
+	}
+	public double getJ(){ return j; }
+	public double getI(){ return i; }
+	public void setJ(double j){ this.j=j; }
+	public void setI(double i){ this.i=i; }
+}
